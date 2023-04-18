fe508dd2dfeb45c836c9233a8629825363d43112
mule-heo <83588163+mule-heo@users.noreply.github.com>
2023-04-19 00:13:57 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -2,14 +2,13 @@ function solution(brown, yellow) {
     // 2n + 2m - 4 = brown 테두리 길이
     // (n-2) * (m-2) = yellow 중앙 영역
     let sum = (brown + yellow);
-    const potentialNM = [];
+    // 개선 - 약수 구하는 과정 생략하고 바로 조건 비교
     for (let i=1;i<=Math.sqrt(sum);i++){
         if (!(sum%i)){
-            potentialNM.push([sum/i, i]);
+            if (2 * sum/i + 2 * i - 4 === brown){
+                return [sum/i, i];
+            }
         }
-    }
-    for (let nm of potentialNM){
-        if (2 * nm[0] + 2 * nm[1] - 4 === brown) return nm;
     }
     // Leo를 믿을 수 없어서 추가 -> 조건을 만족하는 [n, m]이 존재하지 않음
     return false;
