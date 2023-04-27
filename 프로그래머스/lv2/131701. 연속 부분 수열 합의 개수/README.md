75b82ef16e2cb99ede0cf752967801821aa6f7b5
mule-heo <83588163+mule-heo@users.noreply.github.com>
2023-04-27 14:07:53 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 76.54 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/README.md
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속 부분 수열 합의 개수 - 131701 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701?language=javascript) 
 
 ### 성능 요약
 
-메모리: 56.1 MB, 시간: 897.77 ms
+메모리: 57.2 MB, 시간: 76.54 ms
 
 ### 구분
 
--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,16 +1,12 @@
 function solution(elements) {
     const set = new Set();
     const circleElements = [...elements, ...elements];
-    function sum(start, num, arr){
-        let count = 0;
-        for (let i=start;i<start + num;i++){
-            count += arr[i];
-        }
-        return count;
-    }
+
     for (let num=1;num<=elements.length;num++){
+        let count = 0;
         for (let start=0;start<elements.length;start++){
-            set.add(sum(start, num, circleElements));
+            count += circleElements[num + start];
+            set.add(count);
         }
     }
     return set.size;
