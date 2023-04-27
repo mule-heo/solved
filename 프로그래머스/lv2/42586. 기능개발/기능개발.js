0a1b6d2775594daae0769cc1fb8d53ea839c1536
mule-heo <83588163+mule-heo@users.noreply.github.com>
2023-04-27 14:25:54 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,17 +1,15 @@
 function solution(progresses, speeds) {
     const result = [];
-    let done = 0;
-    while (true){
+    while (progresses.length){
         let days = Math.ceil((100 - progresses[0])/speeds[0]);
         progresses = progresses.map((v,i) => v + days * speeds[i]);
-        let index = progresses.findIndex(v => v < 100);
-        if (index === -1) {
-            result.push(progresses.length);
-            return result;
-        } else {
-            result.push(index);
-            progresses = progresses.slice(index);
-            speeds = speeds.slice(index);
+        let done = 0;
+        while (progresses[0] >= 100){
+            done++;
+            progresses.shift();
+            speeds.shift();
         }
-    }
+        result.push(done);
+        }
+    return result; 
 }
