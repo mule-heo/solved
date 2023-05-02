f5aa963e9d78f9003aa08584d8ef0d30a598b7b9
mule-heo <83588163+mule-heo@users.noreply.github.com>
2023-05-03 01:27:20 +0900

    [level 2] Title: 타겟 넘버, Time: 1.68 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/README.md
+++ 프로그래머스/lv2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=javascript) 
 
 ### 성능 요약
 
-메모리: 37.6 MB, 시간: 7.49 ms
+메모리: 35.5 MB, 시간: 1.68 ms
 
 ### 구분
 
--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,12 +1,12 @@
 function solution(numbers, target) {
-    const num = 2 ** numbers.length - 1;
-    let count = 0;
-    for (let i=0;i<num;i++){
-        let truefalse = i.toString(2).padStart(numbers.length, "0");
-        count += +(numbers.reduce((acc, cur, idx) => {
-            if (+truefalse[idx]) return acc + cur;
-            else return acc - cur;
-        }, 0) === target);
+    function add(idx, acc){
+        if (idx === numbers.length) return acc === target;
+        else {
+            let result = 0;
+            result += add(idx+1, acc + numbers[idx]);
+            result += add(idx+1, acc - numbers[idx]);
+            return result;
+        }
     }
-    return count;
+    return add(0, 0);
 }
