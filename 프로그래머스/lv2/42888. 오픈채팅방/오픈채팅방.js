667f3a3351e36447467600a76447ee1046d9cafe
mule-heo <83588163+mule-heo@users.noreply.github.com>
2023-06-04 23:40:54 +0900

    [level 2] Title: 오픈채팅방, Time: 164.01 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,35 +1,23 @@
 function solution(record) {
     let result = [];
-    const idIndex = {};
     const idNickname = {};
-    const idChanged = new Set();
     
     record.forEach(line => {
         let [action, id, nickname] = line.split(" ");
-        switch (action){
-            case "Enter":
-                idNickname[id] = nickname;
-                idIndex[id] ? idIndex[id].push(result.length) : idIndex[id] = [result.length];
-                result.push([nickname, "님이 들어왔습니다."]);
-                if (id in idNickname) idChanged.add(id);
-                break;
-            case "Leave":
-                idIndex[id].push(result.length);
-                result.push([nickname, "님이 나갔습니다."]);
-                break;
-            case "Change":
-                idNickname[id] = nickname;
-                idChanged.add(id);
-                break;
-            default: break;   
+        if (action === "Enter"){
+            idNickname[id] = nickname;
+            result.push([id, "님이 들어왔습니다."]);
+            
         }
-    });
+        else if (action === "Leave"){
+            result.push([id, "님이 나갔습니다."]);
+        } else if (action === "Change"){
+            idNickname[id] = nickname;
+        }
+    })
     
-    Array.from(idChanged).forEach(id => {
-        idIndex[id].forEach(idx => {
-            result[idx][0] = idNickname[id];
-            result[idx] = result[idx].join("");
-        })
-    });
-    return result;
+    return result.map(v => {
+        v[0] = idNickname[v[0]];
+        return v.join("");
+    })
 }
